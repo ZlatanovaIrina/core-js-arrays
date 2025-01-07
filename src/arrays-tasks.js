6d02f07ac97c9e29bee2c51c42c1663bade13cfe
Zlatanova Irina <t0ffy@ya.ru>
2025-01-08 00:27:39 +0200

    feat: implement sumArrays

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -37,8 +37,13 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const [arrFirst, arrSecond] =
+    arr1.length > arr2.length ? [arr1, arr2] : [arr2, arr1];
+
+  return arrFirst.map(
+    (value, index) => value + (arrSecond[index] ? arrSecond[index] : 0)
+  );
 }
 
 /**
