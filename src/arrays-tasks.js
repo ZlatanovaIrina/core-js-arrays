6cdbd563f4c804c4265494be9ed4f0e10a60dd4d
Zlatanova Irina <t0ffy@ya.ru>
2025-01-08 09:48:28 +0200

    feat: implement flattenArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -292,8 +292,11 @@ function createNDimensionalArray(n, size) {
  *    flattenArray(['a', ['b', ['c', 'd'], 'e'], 'f']) => ['a', 'b', 'c', 'd', 'e', 'f']
  *    flattenArray([1, 2, 3, 4]) => [1, 2, 3, 4]
  */
-function flattenArray(/* nestedArray */) {
-  throw new Error('Not implemented');
+function flattenArray(nestedArray) {
+  if (nestedArray.every(($) => !Array.isArray($))) {
+    return nestedArray;
+  }
+  return [].concat(flattenArray(nestedArray.flat()));
 }
 
 /**
